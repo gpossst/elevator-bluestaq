240d0f278374cd70e10c2cfcc281431ae7cfb1cc
gpostVT <gep100iosgames@gmail.com>
2025-10-20 11:00:20 -0400

    hello world

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {   
+        System.out.println("Hello, World!");
+    }
+}
+
+/*
+ * Notes:
+ * - Elevator class is responsible for moving, storing current location
+ * - Control class is responsible for holding location queue, and sending the elevator to the next location
+ */
